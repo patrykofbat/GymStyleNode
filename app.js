569c76ec01d8929ec1f9e1617c96706879afbc9d
Patryk <patrykpapior1997@gmail.com>
2018-08-17 17:45:17 +0200

    simple routing, read from file, exporter router

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ import mysql from "mysql";
 import Exercise from "./models/Exercise";
 import fs from "fs";
 import dbManager from "./database/dbManager";
+import bodyParser from "body-parser";
+import exporter from "./controllers/router/exporter";
 
 
 let app = express();
 
 app.use(cors());
+app.use(bodyParser());
+
+app.use("/export", exporter);
 
 app.listen(8081, () => {
     console.log("Server running on 127.0.0.1");
@@ -17,22 +22,10 @@ app.listen(8081, () => {
 let exercise = new Exercise(100, "siemka", "elo");
 console.log(exercise.parseInsertQuery("gymstyle"));
 
-fs.readFile("./assets/chest.txt","utf8", (err, content)=>{
-    console.log(content);
-});
-console.log("siemka")
+// fs.readFile("./assets/chest.txt","utf8", (err, content)=>{
+//     console.log(content);
+// });
 
 let db = new dbManager("localhost", "root", "gymstyle");
 db.executeQuery("show tables");
 
-// let conn = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     database : 'gymstyle'
-// });
-
-// conn.connect();
-
-// conn.query("show tables", (err, rows, fields)=>{
-//     console.log(rows);
-// });
