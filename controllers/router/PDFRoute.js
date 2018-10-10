f228992490e5df2be3a2bee23a81e6ed89b5adf6
Patryk <patrykpapior1997@gmail.com>
2018-10-10 23:36:52 +0200

    pdf almoste finally found font which support polish characters

--- controllers/router/PDFRoute.js
+++ controllers/router/PDFRoute.js
@@ -6,14 +6,31 @@ import fs from "fs";
 let router  = express.Router();
 
 router.post("/", (req, res) => {
-    let doc = new PDFDocument();
-    doc.pipe(fs.createWriteStream(`./assets/pdf/${req.body[0].nameOfTraining}.pdf`));
-    doc.font('./assets/fonts/Anonymous_Pro.ttf');
+    const {nameOfTraining, exercises} = req.body[0];
+    let y = 100;
 
-    doc.text("Nodeżó js pdf", 100, 100);
+    let doc = new PDFDocument();
+    doc.pipe(fs.createWriteStream(`./assets/pdf/${nameOfTraining}.pdf`));
+    doc.font('./assets/fonts/Anonymous_Pro.ttf');
+    doc.text("Ćwiczenie", 70, 70);
+    doc.text("Serie", 360, 70);
+    doc.text("Powtórzenia", 410, 70);
+    doc.text("Tempo", 500, 70);
+
+
+    exercises.forEach((element, index)=>{
+        const {content, series, tempo, reps} = element;
+        doc.fontSize(8);
+        doc.text((index+1) + "." + content , 70, y);
+        doc.text(series, 360, y);
+        doc.text(reps, 410, y);
+        doc.text(tempo, 500, y);
+        y+=20;
+
+    });
     doc.end();
 
-    res.json({link: `http://localhost:8081/${req.body[0].nameOfTraining}.pdf`});
+    res.json({link: `http://localhost:8081/${nameOfTraining}.pdf`});
 
 });
 
