c4a81b4bd25b7d75d4df8248afc92660891a9f46
patryk <patrykpapior1997@gmail.com>
2018-10-15 23:17:45 +0200

    select by login works

--- controllers/router/RegistrationRoute.js
+++ controllers/router/RegistrationRoute.js
@@ -1,6 +1,8 @@
 import express from "express";
-import crypto from "crypto";
-
+import DbManager from "../../database/DbManager";
+import dotenv from "dotenv";
+import User from "../../models/User";
+dotenv.config();
 
 let router = express.Router();
 
@@ -8,8 +10,31 @@ let router = express.Router();
 router.post('/', (req,res) =>{
    let userData = req.body;
 
-   let userPassword = crypto.createHash('sha256').update(userData.password).digest('hex');
-   console.log(userPassword);
+   const dbManager = new DbManager({
+       host: process.env.DB_HOST,
+       user: process.env.DB_USER,
+       database: process.env.DB_NAME,
+       password: process.env.DB_PASS
+   });
+
+   let user = new User(req.body);
+
+   dbManager.executeQuery(user.parseIntoSelectByLogin("usersgymstyle"),
+       (err, rows, fields)=>{
+       if(!err){
+           if(rows.length !== 0) {
+               console.log(rows);
+               console.log("user exists");
+           }
+
+       }
+
+
+   });
+
+
+
+   console.log(user.parseIntoSelectByLogin("usersgymstyle"));
    res.json({
        message:"Konto zostało utworzone"
    })
--- models/User.js
+++ models/User.js
@@ -1,16 +1,18 @@
+import crypto from "crypto";
 
 export default class User{
 
 
-    constructor(login, password, email){
+    constructor({login, password, email}){
         this.login = login;
         this.password = password;
         this.email = email;
     }
 
     parseIntoInsertQuery(tableName){
+        let password = crypto.createHash('sha256').update(this.password).digest('hex');
         return `INSERT INTO ${tableName} (login, password, email) 
-            VALUES ('${this.login}', '${this.password}', '${this.email}')`;
+            VALUES ('${this.login}', '${password}', '${this.email}')`;
     }
 
     parseIntoSelectByLogin(tableName){
